f8ff9b0cc24f6d02d39688fff26dca3c0bd05ed5
raicem <unalancem@gmail.com>
2025-06-07 19:01:03 +0300

    Add nixos settings

--- programs.nix
+++ programs.nix
@@ -6,6 +6,15 @@
     "Code/User/settings.json".source = ./dotfiles/vscode/settings.json;
     "Code/User/keybindings.json".source = ./dotfiles/vscode/keybindings.json;
   };
+  
+  programs.vscode = {
+    enable = true;
+    mutableExtensionsDir = true;
+  };
+
+  programs.lazygit = {
+    enable = true;
+  };
 
   programs.fish = {
     enable = true;
